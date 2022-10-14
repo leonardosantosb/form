38778fe2aaecb84bc276a2a8914d9165cf891002
Rodrigo Almino <rdg.almino@gmail.com>
2022-10-14 17:07:19 -0300

    Requesito 18

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-const buttonLogin = document.getElementById("loginButton")
-buttonLogin.addEventListener("click",function(){
-  let emailLogin = document.getElementById("email").value
-  let senhaLogin = document.getElementById("password").value
-  if(emailLogin === "tryber@teste.com" && senhaLogin === "123456"){
-    alert("Olá, Tryber!")
-  }else{
-    alert("Email ou senha inválidos.")
+const buttonLogin = document.getElementById("loginButton");
+buttonLogin.addEventListener("click", function () {
+  let emailLogin = document.getElementById("email").value;
+  let senhaLogin = document.getElementById("password").value;
+  if (emailLogin === "tryber@teste.com" && senhaLogin === "123456") {
+    alert("Olá, Tryber!");
+  } else {
+    alert("Email ou senha inválidos.");
   }
-})
+});
+
+const checkboxBtn = document.querySelector("#agreement");
+checkboxBtn.addEventListener("click", () => {
+  // event.preventDefault();
+  const buttonSubmit = document.querySelector("#submit-btn");
+  if (checkboxBtn.checked) buttonSubmit.disabled = false;
+  else buttonSubmit.disabled = true;
+  console.log(checkbox.checked);
+});
